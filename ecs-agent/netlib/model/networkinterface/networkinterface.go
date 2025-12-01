b2a910913bcd437c61aa2309ebfb9a34f0298fb0
Aviral Khattar <avirak@amazon.com>
2025-12-01 20:50:10 +0000

    hard-code device name to eth1 when creating the interface,

--- ecs-agent/netlib/model/networkinterface/networkinterface.go
+++ ecs-agent/netlib/model/networkinterface/networkinterface.go
@@ -622,16 +622,15 @@ func (ni *NetworkInterface) setDeviceName(macToName map[string]string) error {
 		// We don't need to find the name for a branch NetworkInterface as it's just a vlan association.
 		// Get the name of the trunk interface since we need it for constructing the
 		// name of the branch interface.
-		trunkName, ok := macToName[ni.InterfaceVlanProperties.TrunkInterfaceMacAddress]
+		_, ok := macToName[ni.InterfaceVlanProperties.TrunkInterfaceMacAddress]
 		if !ok {
 			// Same as above. Guard against edge-cases where we're unable to find the trunk
 			// NetworkInterface because of internal EC2 errors.
 			return NewUnableToFindENIError(
 				ni.InterfaceVlanProperties.TrunkInterfaceMacAddress, ni.InterfaceAssociationProtocol)
 		}
-		// Name of the branch is based on the vlan id and the name of the trunk.
-		// Example: eth1.24, where trunk is attached as `eth1` and vlan id is `24`.
-		ni.DeviceName = fmt.Sprintf("%s.%s", trunkName, ni.InterfaceVlanProperties.VlanID)
+		// For managed instances, normalize VLAN interface names to eth1 for consistent metrics collection
+		ni.DeviceName = "eth1"
 	default:
 		// Do nothing.
 	}
